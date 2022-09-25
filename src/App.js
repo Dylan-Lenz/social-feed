d0e15c72506dca39964cb469baeb7b891fb6d1f4
unknown <dylanjlenz@gmail.com>
2022-09-25 11:53:03 -0500

    Created InputForm + OutputPost folder + File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import InputForm from './Components/InputForm/InputForm';
 
 function App() {
+
+  const [posts, setPosts] = useState([{name: 'John Smith', post: 'Hello Virtual World'}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <InputForm/>
     </div>
   );
 }
---
+++ src/Components/InputForm/InputForm.jsx
@@ -0,0 +1,13 @@
+
+const InputForm = () => {
+    return (
+        <form>
+            <label>Name:</label>
+            <input type='string'/>
+            <label>Post:</label>
+            <input type='string'/>
+        </form>
+    );
+}
+ 
+export default InputForm;
